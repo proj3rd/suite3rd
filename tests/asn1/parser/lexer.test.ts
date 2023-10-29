729ecc0de0abf94f9f7882c338f2220532d7a87f
Seokseong Jeon <sjeon87@gmail.com>
2023-10-30 07:36:53 +0900

    Add ASN.1 lexer test

---
+++ tests/asn1/parser/lexer.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "vitest";
+import { lexer } from "../../../src/asn1/parser/lexer";
+import { readFileSync, readdirSync } from "fs";
+import { resolve } from "path";
+
+const asn1ResourcePath = "../../../resources/asn1";
+
+describe("ASN.1 lexer", () => {
+  const asn1files = readdirSync(resolve(__dirname, asn1ResourcePath)).filter(
+    (file) => file.endsWith(".asn1")
+  );
+  for (let i = 0; i < asn1files.length; i++) {
+    const asn1file = asn1files[i];
+    test(`Tokenize ${asn1file}`, () => {
+      const content = readFileSync(
+        resolve(__dirname, asn1ResourcePath, asn1file),
+        "utf8"
+      );
+      const tokens = lexer.tokenize(content);
+      expect(tokens.errors.length).toBe(0);
+    });
+  }
+});
