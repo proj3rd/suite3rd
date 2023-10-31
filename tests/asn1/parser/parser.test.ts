45c2fcc48bd51ca896905ccf7a8b3539f7414358
Seokseong Jeon <sjeon87@gmail.com>
2023-10-31 19:01:41 +0900

    Working on parser test

--- tests/asn1/parser/parser.test.ts
+++ tests/asn1/parser/parser.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, test } from "vitest";
 import { Asn1Parser } from "../../../src/asn1/parser/parser";
+import { readFileSync, readdirSync } from "fs";
+import { resolve } from "path";
+import { lexer } from "../../../src/asn1/parser/lexer";
 
 const asn1ResourcePath = "../../../resources/asn1";
 
@@ -8,4 +11,19 @@ describe("ASN.1 parser", () => {
     const parser = new Asn1Parser();
     expect(parser).toBeDefined();
   });
+
+  const asn1files = readdirSync(resolve(__dirname, asn1ResourcePath)).filter(
+    (file) => file.endsWith(".asn1")
+  );
+  for (let i = 0; i < asn1files.length; i++) {
+    const asn1file = asn1files[i];
+    test(`Parsing ${asn1file}`, () => {
+      const content = readFileSync(
+        resolve(__dirname, asn1ResourcePath, asn1file),
+        "utf8"
+      );
+
+      const lexingResult = lexer.tokenize(content);
+    });
+  }
 });
