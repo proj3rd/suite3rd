18639a7a20a6945c60f2325bf9eaa8f1da0ad5a9
Seokseong Jeon <sjeon87@gmail.com>
2023-10-31 20:39:35 +0900

    Update preprocessor

--- src/asn1/parser/preprocessor.ts
+++ src/asn1/parser/preprocessor.ts
@@ -1,9 +1,19 @@
+type PreprocessorOptions = {
+  keep3gppTagComments: boolean;
+};
+
 const PatternAndReplacementList: Array<[RegExp | string, string]> = [
   [/^\s*?--.*$/gm, ""],
 ];
 
-export function preprocess(content: string) {
-  return PatternAndReplacementList.reduce(
+export function preprocess(content: string, options?: PreprocessorOptions) {
+  const patternAndReplacementList = [...PatternAndReplacementList];
+  if (options?.keep3gppTagComments) {
+    throw Error("unimplemented");
+  } else {
+    patternAndReplacementList.push([/--.*--/g, ""], [/--.*$/gm, ""]);
+  }
+  return patternAndReplacementList.reduce(
     (prev: string, item) => prev.replace(...item),
     content
   );
