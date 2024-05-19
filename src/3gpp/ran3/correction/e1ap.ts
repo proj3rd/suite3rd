8c99128cb5e113e40712d8c17c3057d1ddd7a3ff
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2024-05-19 23:53:46 +0900

    E1

--- src/3gpp/ran3/correction/e1ap.ts
+++ src/3gpp/ran3/correction/e1ap.ts
@@ -2,16 +2,19 @@ import { compare } from "../../../3gpp/version.js";
 import { TestCase } from "./testCase.js";
 
 const SPEC_NUM = "38463";
+const SPEC_NUM_R17 = "37483";
 
 export const testCases: TestCase[] = [
   (definitions, specNum, version) => {
     // Maximum Integrity Protected Data Rate IE (9.3.1.57) should exist
     const VERSION: [number, number, number] = [15, 2, 0];
+    const VERSION_R17: [number, number, number] = [17, 0, 0];
     const CLAUSE = "9.3.1.57";
-    if (specNum !== SPEC_NUM) {
-      return;
-    }
-    if (compare(version, VERSION) < 0) {
+    if (
+      ((specNum !== SPEC_NUM || compare(version, VERSION) < 0) &&
+        specNum !== SPEC_NUM_R17) ||
+      compare(version, VERSION_R17) < 0
+    ) {
       return;
     }
     const definition = definitions.findDefinition(CLAUSE);
@@ -24,11 +27,13 @@ export const testCases: TestCase[] = [
   (definitions, specNum, version) => {
     // Redundant QoS Flow Indicator IE (9.3.1.74) should exist
     const VERSION: [number, number, number] = [16, 2, 0];
+    const VERSION_R17: [number, number, number] = [17, 0, 0];
     const CLAUSE = "9.3.1.74";
-    if (specNum !== SPEC_NUM) {
-      return;
-    }
-    if (compare(version, VERSION) < 0) {
+    if (
+      ((specNum !== SPEC_NUM || compare(version, VERSION) < 0) &&
+        specNum !== SPEC_NUM_R17) ||
+      compare(version, VERSION_R17) < 0
+    ) {
       return;
     }
     const definition = definitions.findDefinition(CLAUSE);
@@ -44,14 +49,16 @@ export const testCases: TestCase[] = [
      * From 16.3.0
      */
     const VERSION: [number, number, number] = [16, 3, 0];
+    const VERSION_R17: [number, number, number] = [17, 0, 0];
     const CLAUSE = "9.2.3.3";
     const IE_NAME = "Trace Collection Entity IP Address";
     const PATTERN_TO_TEST = /\b9\.3\.2\.4\b/;
     const REPLACEMENT = "9.3.2.4";
-    if (specNum !== SPEC_NUM) {
-      return;
-    }
-    if (compare(version, VERSION) < 0) {
+    if (
+      ((specNum !== SPEC_NUM || compare(version, VERSION) < 0) &&
+        specNum !== SPEC_NUM_R17) ||
+      compare(version, VERSION_R17) < 0
+    ) {
       return;
     }
     const definition = definitions.findDefinition(CLAUSE);
